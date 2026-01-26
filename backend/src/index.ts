de04e2ab1bd8046aca092daea1f804fe984c793d
Sana Bulbule <sanabulbule@mac.lan>
2026-01-26 13:56:10 -0600

    server & index changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,26 +1,6 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import monitorRoutes from './routes/apiMonitor';
-
-const app = express();
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Sample route
-app.get('/', (_req: Request, res: Response) => {
-  res.send('Hello, World!');
-});
-
-app.get('/health', (_req: Request, res: Response) => {
-  res.json({ status: 'Active', timestamp: new Date() });
-});
-
-app.get("/debug", (_, res) => {
-  res.send("debug alive");
-});
-
-app.use('/api', monitorRoutes);
+import { startServer } from './server';
+import app from './server';
 
 export default app;
+
+startServer();
